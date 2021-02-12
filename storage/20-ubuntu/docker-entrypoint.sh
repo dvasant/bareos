6ec25cfd7a8fdfabf0833e3c65502b9b933f4984
dvasanth <dvasanth@appranix.com>
2021-02-12 13:27:45 +0530

    bug fix and gfuse installation update

--- storage/20-ubuntu/docker-entrypoint.sh
+++ storage/20-ubuntu/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bareos_sd_config="/etc/bareos/bareos-sd.d/director/bareos-dir.conf"
-
+mount_command="${GCSFUSE_BUCKET} /var/lib/bareos/storage gcsfuse rw,nonempty,_netdev,allow_other,uid=101,gid=101"
 if [ ! -f /etc/bareos/bareos-config.control ]; then
   tar xfz /bareos-sd.tgz --backup=simple --suffix=.before-control
 
@@ -12,9 +12,18 @@ if [ ! -f /etc/bareos/bareos-config.control ]; then
   touch /etc/bareos/bareos-config.control
 fi
 
+if [[ ! -z "${GCSFUSE_BUCKET}" ]]; then
+
+   #Mount bucket to /var/lib/bareos/storage
+   gcsfuse --uid 101 --gid 101 -o allow_other --limit-bytes-per-sec "-1" --limit-ops-per-sec "-1" --stat-cache-ttl "1h" --type-cache-ttl "1h"  $GCSFUSE_BUCKET /var/lib/bareos/storage/
+   
+   #allow_other users
+   sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
+fi
+
 # Fix permissions
 find /etc/bareos/bareos-sd.d ! -user bareos -exec chown bareos {} \;
-chown -R bareos /var/lib/bareos /dev/[n]st* /dev/tape
+chown -R bareos /var/lib/bareos 
 
 # Run Dockerfile CMD
 exec "$@"
