b71e2ba33c296641e019714fb098d974f14e312b
dvasanth <dvasanth@appranix.com>
2021-02-19 12:16:59 +0530

    bug fix

--- storage/20-ubuntu/docker-entrypoint.sh
+++ storage/20-ubuntu/docker-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 bareos_sd_config="/etc/bareos/bareos-sd.d/director/bareos-dir.conf"
-mount_command="${GCSFUSE_BUCKET} /var/lib/bareos/storage gcsfuse rw,nonempty,_netdev,allow_other,uid=101,gid=101"
+# mount_command="${GCSFUSE_BUCKET} /var/lib/bareos/storage gcsfuse rw,nonempty,_netdev,allow_other,uid=101,gid=101"
 if [ ! -f /etc/bareos/bareos-sd-config.control ]; then
   tar xfz /bareos-sd.tgz --backup=simple --suffix=.before-control
 
   # Update bareos-storage configs
   sed -i 's#Password = .*#Password = '\""${BAREOS_SD_PASSWORD}"\"'#' $bareos_sd_config
+
+  sed -i 's#Maximum Concurrent Jobs =.*#Maximum Concurrent Jobs = 10000#' \
+    /etc/bareos/bareos-sd.d/storage/bareos-sd.conf
 
   # Control file
   touch /etc/bareos/bareos-sd-config.control
@@ -15,7 +18,8 @@ fi
 if [[ ! -z "${GCSFUSE_BUCKET}" ]]; then
 
    #Mount bucket to /var/lib/bareos/storage
-   gcsfuse --uid 101 --gid 101 -o allow_other --limit-bytes-per-sec "-1" --limit-ops-per-sec "-1" --stat-cache-ttl "1h" --type-cache-ttl "1h"  $GCSFUSE_BUCKET /var/lib/bareos/storage/
+   gcsfuse --uid 101 --gid 101 -o allow_other --limit-bytes-per-sec "-1" --limit-ops-per-sec "-1" \
+    --stat-cache-ttl "1h" --type-cache-ttl "1h"  $GCSFUSE_BUCKET /var/lib/bareos/storage/
    
    #allow_other users
    sed -i 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
